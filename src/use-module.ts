1bd568f44be0f9b41e110450ebfe8555750b4d56
elzup <guild0105@gmail.com>
2022-02-26 20:20:22 +0900

    babel config

--- src/use-module.ts
+++ src/use-module.ts
@@ -1,8 +1,10 @@
 import { readFileSync } from 'fs'
+import { sub } from './sub'
 
 async function main() {
   const a = readFileSync('./sample.json', 'utf8')
   console.log(a.split('\n'))
+  console.log(sub())
 }
 
 main().then(() => console.log('end'))
